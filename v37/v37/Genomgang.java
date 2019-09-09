29915604f9e395633c4e6908be95b44443a0e894
Scoinc <david_utbult@live.se>
2019-09-09 13:18:55 +0200

    v37
    måndag lektion

---
+++ v37/v37/Genomgang.java
@@ -0,0 +1,57 @@
+package v37;
+
+public class Genomgang {
+
+	public static void main(String[] args) {
+		/* Creeper, aww man. So we back in the mine, got our pickaxe swinging from side to side, side side to side
+		 * this task a grueling one, hope to find some diamonds tonight night night, diamonds tonight.
+		 * Heads up, you hear a sound, turn around and look up, total shock fills your body.
+		 * Oh no it's you again, i could never forget those eyes eyes eyes, eyes eyes eyes eyes.
+		 * Cause baby tonight, the creepers tryna steal all our stuff again, stuff again.
+		 * Cause baby tonight, you grab your pick, shovel and bolt again, bolt again gain.
+		 * And run run, until its done done, until the sun comes up in the morn'.
+		 * Cause baby tonight, the creeper's tryna steal all our stuff again, stuff again gain.
+		 * Just when you think you're safe, overhear some hissing from right behind, right right behind.
+		 * That's a nice life you have, shame it's gonna end at this time time time, time time time time.
+		 * Blows up, and your healthbar drops and you could use a one up. Get inside, don't be tardy.
+		 * So now you're stuck in there, half a heart is left but don't die die die, die die die die.
+		 * Cause baby tonight, the creeper's tryna steal all our stuff again, stuff again.
+		 * Cause baby tonight, you grab your pick, shovel and bolt again, bolt again gain.
+		 * And run run, until it's done done, until the sun comes up in the morn.
+		 * Cause baby tonight, the creeper's tryna steal all our stuff again.
+		 * Dig up diamonds and craft those diamonds, make some armor, get it baby.
+		 * Go and forge that like you so MLG pro the swords made of diamonds so come at me bro.
+		 * Look at me look at you, take my revenge that's what i'm gonna do, i'm a warrior baby what else is new?
+		 * and my sword's gonna tear through you (bring it)
+		 * Cause baby tonight, the creepers tryna steal all our stuff again, stuff again.
+		 * Yeah baby tonight, grab your sword armor and go. Take your revenge.
+		 * So fight, fight like it's the last last night of your life, life show them your bite.
+		 * Cause baby tonight, the creepers tryna steal all our stuff again.
+		 * Cause baby tonight, you grab your pick, shovel and bolt again, bolt again gain.
+		 * And run run, untill it's done done, untill the sun comes up in the morn.
+		 * Cause baby tonight, the creeper tried to steal all our stuff again.
+		 */
+		
+		int heltal = 5; //skapar "heltal" och fyller den med 5
+		int svar; //deklarera
+		
+		svar = heltal*heltal;
+		
+				
+		System.out.println("svar " + heltal*heltal);
+
+		double pi = 3.14;
+		
+		double radie = 5;
+		
+		double area = pi*radie*radie;
+		
+		System.out.println("svar " + area); //cm^2
+		
+		
+				
+				
+				
+	}
+
+}
---
+++ v37/v37/egettest.java
@@ -0,0 +1,12 @@
+package v37;
+
+public class egettest {
+	public static void main(String[] args) {
+		int height = 5; //cube height
+		
+		int width = 5; //cube width
+		
+		System.out.println("svar " + height*width);
+	}
+
+}
