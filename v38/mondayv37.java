adc387a22c59fe98ec6c6b65086eda8e0e97fcbc
Scoinc <david_utbult@live.se>
2019-09-16 13:14:45 +0200

    Create mondayv37.java

---
+++ v38/mondayv37.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class mondayv37 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean again = true;	//Jag orkar inte, ignorera denna
+		int age = 1;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("hur gammal du?");
+		
+		age = input.nextInt();
+		
+		if (age >= 18) {
+		System.out.println("myndig man");
+		}
+		
+		else if (age >= 14 && age <= 18) {
+			System.out.println("ung man men moppe");
+		}
+		
+		else {
+			System.out.println("bara ung man");
+		}
+		
+	}
+
+}
