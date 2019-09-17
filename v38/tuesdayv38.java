05e9395a3932530148af9ac690b97d4387926b1c
Scoinc <david_utbult@live.se>
2019-09-17 12:17:40 +0200

    Create tuesdayv38.java
    kommentarer senare eventuellt

---
+++ v38/tuesdayv38.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class tuesdayv38 {
+
+	public static void main(String[] args) {
+		
+		int tal1 = 0;
+		int tal2 = 0;
+		int tal3 = 0;
+		int tal4 = 0;
+		int tal5 = 0;
+
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Skriv 5 tal");
+		
+		tal1 = input.nextInt();
+		
+		tal2 = input.nextInt();
+		
+		tal3 = input.nextInt();
+		
+		tal4 = input.nextInt();
+		
+		tal5 = input.nextInt();
+		
+		double mid = (tal1+tal2+tal3+tal4+tal5); //medelvärde
+		double mid2 = mid/5;
+		
+		if(tal1 >= tal2 && tal1 >= tal3 && tal1 >= tal4 && tal1 >= tal5){
+		System.out.println("högst tal: "+tal1);
+		}
+		else if(tal2 >= tal1 && tal2 >= tal3 && tal2 >= tal4 && tal2 >= tal5){
+		System.out.println("högst tal: "+tal2);
+		}
+		else if(tal3 >= tal1 && tal3 >= tal2 && tal3 >= tal4 && tal3 >= tal5){
+		System.out.println("högst tal: "+tal3);
+		}
+		else if(tal4 >= tal1 && tal4 >= tal2 && tal4 >= tal3 && tal4 >= tal5){
+		System.out.println("högst tal: "+tal4);
+		}
+		else if(tal5 >= tal1 && tal5 >= tal2 && tal5 >= tal3 && tal5 >= tal4){
+		System.out.println("högst tal: "+tal5);
+		}
+		
+		if(tal1 <= tal2 && tal1 <= tal3 && tal1 <= tal4 && tal1 <= tal5){
+		System.out.println("lägst tal: "+tal1);
+		}
+		else if(tal2 <= tal1 && tal2 <= tal3 && tal2 <= tal4 && tal2 <= tal5){
+		System.out.println("lägst tal: "+tal2);
+		}
+		else if(tal3 <= tal1 && tal3 <= tal2 && tal3 <= tal4 && tal3 <= tal5){
+		System.out.println("lägst tal: "+tal3);
+		}
+		else if(tal4 <= tal1 && tal4 <= tal2 && tal4 <= tal3 && tal4 <= tal5){
+		System.out.println("lägst tal: "+tal4);
+		}
+		else if(tal5 <= tal1 && tal5 <= tal2 && tal5 <= tal3 && tal5 <= tal4){
+		System.out.println("lägst tal: "+tal5);
+		}
+		
+	
+		
+		System.out.println("Medelvärde: "+mid2);
+	}
+
+}
