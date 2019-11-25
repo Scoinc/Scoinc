e304765a5b5fb867cf91f9293368b7b0c163fb79
Scoinc <david_utbult@live.se>
2019-11-25 13:07:17 +0100

    det gick åt helvete

---
+++ v48/Arrays.java
@@ -0,0 +1,15 @@
+public class Arrays {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] numbers1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int[] numbers2 = new int[10];
+
+		for (int i = 0; i < numbers1.length; i++) {
+			System.out.print(numbers1[i]);
+			System.out.println();
+		}
+	}
+}
---
+++ v48/StringArrays.java
@@ -0,0 +1,17 @@
+
+public class StringArrays {
+
+	public static void main(String[] args) {
+
+		String[] namn = { "bavid", "bacob", "bachris", "bandreas" };
+
+		for (int i = 0; i < 40; i++) {
+
+			for (int j = 0; j < namn.length; j++) {
+
+				
+				System.out.println((i) + ". " + namn[j]);
+			}
+		}
+	}
+}
